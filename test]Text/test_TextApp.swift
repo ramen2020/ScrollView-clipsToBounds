1cf072ba06d3a1cc770b3f65c7f4e578885e0328
ramen2020 <vfvsfv.dlk22@gmail.com>
2023-03-16 16:50:13 +0900

    Initial Commit

---
+++ test]Text/test_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_TextApp.swift
+//  test]Text
+//
+//  Created by nao on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct test_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
